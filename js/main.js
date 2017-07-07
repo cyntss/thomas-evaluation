eba8e29a963a5a63511230e78488b1825ede5e53
thomas85wagner <thomas.85.wagner@gmail.com>
2017-07-07 16:12:30 +0200

    impressive figure animation

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var figures = $('.figure');
+
+function init(){
+	countFiguresUp();
+}
+
+function countFiguresUp(){
+	figures.each(function(){
+		var figure = $(this);
+		var figureTargetValue = $(this).data('binding');
+
+		$({countNum: 0}).animate({countNum: figureTargetValue}, {
+		  duration: 500,
+		  easing:'linear',
+		  step: function() {
+		    figure.text(Math.floor(this.countNum));
+		  }
+		});
+	});
+}
+
+init();
