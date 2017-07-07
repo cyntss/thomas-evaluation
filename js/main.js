dba8b5419e7ffec92d6301a9cdf26a1138ccb479
Cynthia Sanchez <csanchez@suse.com>
2017-07-07 18:58:31 +0200

    repo improved

--- js/main.js
+++ js/main.js
@@ -1,25 +1,14 @@
-var figures = $('.figure');
-
-function init(){
-	countFiguresUp();
-}
-
-function countFiguresUp(){
-	figures.each(function(){
-		var figure = $(this);
-		var figureTargetValue = $(this).data('binding');
-
-		$({countNum: 0}).animate({countNum: figureTargetValue}, {
+// Improved
+$(document).on('ready', function() {
+	$('.figure').each(function () {
+		var $figure = $(this);
+		var figureValue = $(this).text()
+		$figure.prop('countNum',0).animate({countNum: figureValue}, {
 			duration: 500,
-			easing:'linear',
-			step: function() {
-				figure.text(Math.floor(this.countNum));
-			},
-			complete: function() {
-				// do highlight figures to @Orange for ~200ms
+			easing: 'linear',
+			step: function(now) {
+				$figure.text(Math.floor(now));
 			}
 		});
 	});
-}
-
-init();
+})
